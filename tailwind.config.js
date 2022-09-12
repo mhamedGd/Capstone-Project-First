0961cac3eb03bf8f593e52ec0b1bcb9fa62d1982
mhamedGd <mhamed.m14@gmail.com>
2022-09-12 14:56:18 +0300

    Initial headline

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*html"],
+  theme: {
+    extend: {
+      colors: {
+        "custom-blue": "#00baff",
+        "custom-purple": "#6336fa",
+      }
+    },
+    fontFamily: {
+      Lato: ["font-family: 'Lato', sans-serif;"],
+    },
+  },
+  plugins: [],
+}
