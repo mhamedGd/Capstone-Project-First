16ead39dce80e6c830a234812e3d4a4506fcd85d
mhamedGd <mhamed.m14@gmail.com>
2022-09-13 20:25:24 +0300

    Add mobile-menu

--- src/script/main.js
+++ src/script/main.js
@@ -1,7 +1,70 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
 
-function CreateSpeaker() {
+const SPEAKERS_INFORMATION = [
+  {
+    image: './rsc/Images/avatar.jpg',
+    name: 'Speaker No.1',
+    info: 'Head of speaking department on mars',
+    paragraph: 'This is a paragraph to prove that I did say something about something blah blah blah blah blah',
+  },
+  {
+    image: './rsc/Images/avatar.jpg',
+    name: 'Speaker No.2',
+    info: 'Head of speaking department in gargantua',
+    paragraph: 'This is a paragraph to prove that I did say something about something blah blah blah blah blah',
+  },
+  {
+    image: './rsc/Images/avatar.jpg',
+    name: 'Bob',
+    info: 'King of the universe',
+    paragraph: 'This is a paragraph to prove that I did say something about something blah blah blah blah blah',
+  },
+  {
+    image: './rsc/Images/avatar.jpg',
+    name: 'Speaker No.4',
+    info: 'a friend of bob',
+    paragraph: 'This is a paragraph to prove that I did say something about something blah blah blah blah blah',
+  },
+  {
+    image: './rsc/Images/avatar.jpg',
+    name: 'Speaker No.5',
+    info: 'Head of speaking department of the lizard people',
+    paragraph: 'This is a paragraph to prove that I did say something about something blah blah blah blah blah',
+  },
+  {
+    image: './rsc/Images/avatar.jpg',
+    name: 'Speaker No.6',
+    info: 'Head of speaking department in the outer universe',
+    paragraph: 'This is a paragraph to prove that I did say something about something blah blah blah blah blah',
+  },
+  {
+    image: './rsc/Images/avatar.jpg',
+    name: 'Speaker No.7',
+    info: 'Head of speaking department in andromeda galaxy',
+    paragraph: 'This is a paragraph to prove that I did say something about something blah blah blah blah blah',
+  },
+  {
+    image: './rsc/Images/avatar.jpg',
+    name: 'Speaker No.8',
+    info: 'Head of speaking department in mariana trench',
+    paragraph: 'This is a paragraph to prove that I did say something about something blah blah blah blah blah',
+  },
+  {
+    image: './rsc/Images/avatar.jpg',
+    name: 'Speaker No.9',
+    info: 'Head of speaking department on the sun',
+    paragraph: 'This is a paragraph to prove that I did say something about something blah blah blah blah blah',
+  },
+  {
+    image: './rsc/Images/avatar.jpg',
+    name: 'Speaker No.10',
+    info: 'Head of speaking department on pluto',
+    paragraph: 'This is a paragraph to prove that I did say something about something blah blah blah blah blah',
+  },
+];
+
+function CreateSpeaker(SPEAKER_INFO_OBJECT) {
   const SPEAKER_MAIN = document.createElement('li');
   SPEAKER_MAIN.classList.add('flex');
   SPEAKER_MAIN.classList.add('gap-x-5');
@@ -14,10 +77,12 @@ function CreateSpeaker() {
   IMAGE_DIV.classList.add('flex');
   IMAGE_DIV.classList.add('justify-center');
   IMAGE_DIV.classList.add('items-center');
+  IMAGE_DIV.classList.add('max-w-[150px]');
+  IMAGE_DIV.classList.add('max-h-[150px]');
   SPEAKER_MAIN.appendChild(IMAGE_DIV);
 
   const IMAGE = document.createElement('img');
-  IMAGE.src = './rsc/Images/avatar.jpg';
+  IMAGE.src = SPEAKER_INFO_OBJECT.image;
   IMAGE.classList.add('min-w-[100px]');
   IMAGE.classList.add('min-h-[100px]');
   IMAGE.classList.add('max-w-[300px]');
@@ -36,14 +101,14 @@ function CreateSpeaker() {
   const SPEAKER_NAME = document.createElement('h3');
   SPEAKER_NAME.classList.add('text-2xl');
   SPEAKER_NAME.classList.add('font-bold');
-  SPEAKER_NAME.textContent = 'Speaker';
+  SPEAKER_NAME.textContent = SPEAKER_INFO_OBJECT.name;
   INFO_DIV_DIV.appendChild(SPEAKER_NAME);
 
   const SPEAKER_INFO = document.createElement('p');
   SPEAKER_INFO.classList.add('text-orange-400');
   SPEAKER_INFO.classList.add('text-xl');
   SPEAKER_INFO.classList.add('mt-2.5');
-  SPEAKER_INFO.textContent = 'This is orange';
+  SPEAKER_INFO.textContent = SPEAKER_INFO_OBJECT.info;
   INFO_DIV_DIV.appendChild(SPEAKER_INFO);
 
   const LINE = document.createElement('hr');
@@ -55,10 +120,18 @@ function CreateSpeaker() {
 
   const SPEAKER_PARAGRAPH = document.createElement('p');
   SPEAKER_PARAGRAPH.classList.add('mt-3');
-  SPEAKER_PARAGRAPH.textContent = 'Listen to the speakers from various countries about the messages of sharing and opening.';
+  SPEAKER_PARAGRAPH.textContent = SPEAKER_INFO_OBJECT.paragraph;
   INFO_DIV_DIV.appendChild(SPEAKER_PARAGRAPH);
 
   document.querySelector('.speakers').appendChild(SPEAKER_MAIN);
 }
 
-CreateSpeaker();
+for (let i = 0; i < 9; i += 1) {
+  CreateSpeaker(SPEAKERS_INFORMATION[i]);
+}
+
+const MENU_BUTTON = document.querySelector('.menu-button');
+
+MENU_BUTTON.addEventListener('click', () => {
+  document.querySelector('.menu-content').classList.toggle('open');
+});
